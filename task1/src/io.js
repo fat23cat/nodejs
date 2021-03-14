2657aa6ba6f5ed23db79e1e4aba4ca1d6760b017
Pavel Sidorenko <fat.cat.1703@gmail.com>
2021-03-14 17:36:07 +0300

    refactor

--- task1/src/io.js
+++ task1/src/io.js
@@ -1,6 +1,7 @@
 export function ioObserver() {
-  process.stdin.on("data", (buffer) => {
+  const { stdin, stdout } = process;
+  stdin.on("data", (buffer) => {
     const reversed = buffer.toString().split("").reverse().join("");
-    process.stdout.write(reversed + "\n\n");
+    stdout.write(reversed + "\n\n");
   });
 }
