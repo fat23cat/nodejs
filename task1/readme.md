c7c77d4a889e02a086e405a63ca3fb2145bdc296
Pavel Sidorenko <fat.cat.1703@gmail.com>
2021-03-18 10:11:50 +0300

    add task2 and readme

---
+++ task1/readme.md
@@ -0,0 +1,14 @@
+# Homework 1
+
+## Install dependencies
+In the `/task1` directory run `npm i`
+
+## Task 1
+The program reads a string from the standard input stdin, reverses it and then writes it to the standard output stdout.
+
+To start the program In the `/task1` directory run `npm run task1`
+
+## Task 2
+The program reads a file from the `/csv` directory, and converts it to `.txt` file.
+
+To start the program In the `/task2` directory run `npm run task2`
--- task1/src/task2/transformer.js
+++ task1/src/task2/transformer.js
@@ -1,3 +1,23 @@
+import csv from "csvtojson";
+import fs from "fs";
+import path from "path";
+
 export function transform() {
-  console.log("transform");
+  const { stdout } = process;
+
+  const inputFilePath = path.join(__dirname, "csv", "data.csv");
+  const outputFilePath = path.join(__dirname, "txt", "data.txt");
+
+  const readStream = fs.createReadStream(inputFilePath);
+  const writeStream = fs.createWriteStream(outputFilePath);
+
+  readStream
+    .pipe(csv())
+    .on("error", (err) => {
+      stdout.write(err);
+    })
+    .pipe(writeStream)
+    .on("error", (err) => {
+      stdout.write(err);
+    });
 }
