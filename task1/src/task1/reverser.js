a78bd1a0f418359f58c769020a39fa7e614347d1
Pavel Sidorenko <fat.cat.1703@gmail.com>
2021-03-19 16:14:52 +0300

    fix

--- task1/src/task1/reverser.js
+++ task1/src/task1/reverser.js
@@ -1,5 +1,6 @@
 export function reverse() {
   const { stdin, stdout } = process;
+
   stdin.on("data", (buffer) => {
     const reversed = buffer.toString().split("").reverse().join("");
     stdout.write(reversed + "\n\n");
