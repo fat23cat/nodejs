89961d0449ac68d527d6446e326041a11db975df
Pavel Sidorenko <fat.cat.1703@gmail.com>
2021-03-28 14:52:28 +0300

    fix

--- api/src/app.js
+++ api/src/app.js
@@ -1,1 +1,10 @@
-console.log('start');
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+const port = 8080;
+
+app.listen(port, () => {
+  const { stdout } = process;
+  stdout.write(`Server is running on port ${port}`);
+});
---
+++ api/src/fakeData/fakeData.js
@@ -0,0 +1,37 @@
+export const users = [
+  {
+    id: '1',
+    login: 'Alex',
+    password: 'password',
+    age: 10,
+    isDeleted: false
+  },
+  {
+    id: '2',
+    login: 'Alexander',
+    password: 'password',
+    age: 11,
+    isDeleted: false
+  },
+  {
+    id: '3',
+    login: 'Alexa',
+    password: 'password',
+    age: 12,
+    isDeleted: false
+  },
+  {
+    id: '4',
+    login: 'Paul',
+    password: 'password',
+    age: 13,
+    isDeleted: false
+  },
+  {
+    id: '5',
+    login: 'Anna',
+    password: 'password',
+    age: 14,
+    isDeleted: true
+  }
+];
